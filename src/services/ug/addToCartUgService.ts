c3e869f6b9a5591f527f5eb6aa2b6127e011266f
dkyzym <kyzym12345@gmail.com>
2024-10-30 18:58:28 +0100

    refactor: add to cart ug

--- src/services/ug/addToCartUgService.ts
+++ src/services/ug/addToCartUgService.ts
@@ -38,16 +38,74 @@ export const addToCartUgService = async (
     }
 
     await inputElement.click();
-
     await inputElement.type(count.toString());
-
     await buyButton.click();
     console.log('Кнопка "Купить" нажата.');
 
-    return {
-      message: `${item.brand} ${item.article} успешно добавлен`,
-      success: true,
-    };
+    // Ждем появления либо диалогового окна, либо тултипа
+    await page.waitForFunction(
+      () => {
+        return (
+          document.querySelector('div.ui-dialog') ||
+          document.querySelector('div.ui-tooltip-content')
+        );
+      },
+      { timeout: 5000 }
+    );
+
+    const modalDialog = await page.$('div.ui-dialog');
+
+    if (modalDialog) {
+      // Если появилось диалоговое окно, находим кнопку "Да" и нажимаем
+      const buttons = await modalDialog.$$('button');
+      let yesButtonFound = false;
+      for (const button of buttons) {
+        const buttonText = await page.evaluate(
+          (el) => el.textContent?.trim(),
+          button
+        );
+        if (buttonText === 'Да') {
+          await button.click();
+          console.log('Нажата кнопка "Да" в диалоговом окне.');
+          yesButtonFound = true;
+          break;
+        }
+      }
+      if (!yesButtonFound) {
+        throw new Error('Не найдена кнопка "Да" в диалоговом окне.');
+      }
+      // Возвращаем успех без ожидания тултипа
+      return {
+        message: `${item.brand} ${item.article} успешно добавлен`,
+        success: true,
+      };
+    } else {
+      // Если диалогового окна нет, ждем появления тултипа
+      await page.waitForSelector('div.ui-tooltip-content', { timeout: 5000 });
+
+      const tooltipText = await page.$eval('div.ui-tooltip-content', (el) =>
+        el.textContent?.trim()
+      );
+
+      if (tooltipText === 'Товар добавлен в корзину') {
+        console.log(
+          'Тултип подтверждения добавления товара в корзину обнаружен.'
+        );
+        return {
+          message: `${item.brand} ${item.article} успешно добавлен`,
+          success: true,
+        };
+      } else {
+        console.log(
+          'Тултип подтверждения добавления товара в корзину не соответствует ожиданиям.'
+        );
+      }
+
+      return {
+        message: `Товар не добавлен в корзину`,
+        success: false,
+      };
+    }
   } catch (error) {
     logger.error(`${supplier}: Error in addToCart:`, (error as Error).stack);
     return {
