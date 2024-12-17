3bc9dfcf55c5e2241821a32778871f9a4a02f30d
dkyzym <kyzym12345@gmail.com>
2024-12-17 09:05:20 +0100

    chore: suppliers config

--- src/utils/calculateDates/suppliersConfig/suppliersConfig.ts
+++ src/utils/calculateDates/suppliersConfig/suppliersConfig.ts
@@ -2,87 +2,87 @@ import { DateTime } from 'luxon';
 import { SearchResultsParsed, SupplierConfig } from 'types';
 
 export const suppliersConfig: SupplierConfig[] = [
-  // {
-  //   supplierName: 'patriot',
-  //   workingDays: [2, 3, 4, 5, 6], // со вторника по субботу
-  //   cutoffTimes: {
-  //     default: '11:30', // Начальный крайний срок
-  //     extended: '20:00', // Второй крайний срок
-  //   },
-  //   processingTime: { days: 0 },
-  //   specialConditions: (currentTime: DateTime, result: SearchResultsParsed) => {
-  //     let deliveryDate: DateTime;
+  {
+    supplierName: 'patriot',
+    workingDays: [2, 3, 4, 5, 6], // со вторника по субботу
+    cutoffTimes: {
+      default: '11:30', // Начальный крайний срок
+      extended: '20:00', // Второй крайний срок
+    },
+    processingTime: { days: 0 },
+    specialConditions: (currentTime: DateTime, result: SearchResultsParsed) => {
+      let deliveryDate: DateTime;
 
-  //     if (
-  //       currentTime.hour < 11 ||
-  //       (currentTime.hour === 11 && currentTime.minute < 30)
-  //     ) {
-  //       // Заказ до 11:30 - доставка сегодня
-  //       deliveryDate = currentTime;
-  //     } else if (currentTime.hour < 20) {
-  //       // Заказ между 11:30 и 20:00 - доставка на следующий день
-  //       deliveryDate = currentTime.plus({ days: 1 });
-  //     } else {
-  //       // Заказ после 20:00 - доставка через два дня
-  //       deliveryDate = currentTime.plus({ days: 2 });
-  //     }
+      if (
+        currentTime.hour < 11 ||
+        (currentTime.hour === 11 && currentTime.minute < 30)
+      ) {
+        // Заказ до 11:30 - доставка сегодня
+        deliveryDate = currentTime;
+      } else if (currentTime.hour < 20) {
+        // Заказ между 11:30 и 20:00 - доставка на следующий день
+        deliveryDate = currentTime.plus({ days: 1 });
+      } else {
+        // Заказ после 20:00 - доставка через два дня
+        deliveryDate = currentTime.plus({ days: 2 });
+      }
 
-  //     // Добавляем специальные условия для warehouse
-  //     if (
-  //       result.warehouse.includes('Донецк') ||
-  //       result.warehouse.includes('Мариуполь')
-  //     ) {
-  //       deliveryDate = deliveryDate.plus({ days: 2 });
-  //     } else if (result.warehouse.includes('Мелитополь')) {
-  //       deliveryDate = deliveryDate.plus({ days: 3 });
-  //     }
+      // Добавляем специальные условия для warehouse
+      if (
+        result.warehouse.includes('Донецк') ||
+        result.warehouse.includes('Мариуполь')
+      ) {
+        deliveryDate = deliveryDate.plus({ days: 2 });
+      } else if (result.warehouse.includes('Мелитополь')) {
+        deliveryDate = deliveryDate.plus({ days: 3 });
+      }
 
-  //     // Корректировка для дней без доставки (воскресенье и понедельник)
-  //     while (deliveryDate.weekday === 1 || deliveryDate.weekday === 7) {
-  //       deliveryDate = deliveryDate.plus({ days: 1 });
-  //     }
+      // Корректировка для дней без доставки (воскресенье и понедельник)
+      while (deliveryDate.weekday === 1 || deliveryDate.weekday === 7) {
+        deliveryDate = deliveryDate.plus({ days: 1 });
+      }
 
-  //     return deliveryDate;
-  //   },
-  // },
-  // {
-  //   supplierName: 'ug',
-  //   workingDays: [1, 4], // понедельник и четверг
-  //   cutoffTimes: {
-  //     default: '14:00',
-  //   },
-  //   processingTime: { days: 0 },
-  //   specialConditions: (currentTime: DateTime, result: SearchResultsParsed) => {
-  //     let deliveryDate: DateTime;
+      return deliveryDate;
+    },
+  },
+  {
+    supplierName: 'ug',
+    workingDays: [1, 4], // понедельник и четверг
+    cutoffTimes: {
+      default: '14:00',
+    },
+    processingTime: { days: 0 },
+    specialConditions: (currentTime: DateTime, result: SearchResultsParsed) => {
+      let deliveryDate: DateTime;
 
-  //     const getNextDeliveryDateFrom = (date: DateTime): DateTime => {
-  //       let nextDate = date;
-  //       while (true) {
-  //         if (nextDate.weekday === 1 || nextDate.weekday === 4) {
-  //           return nextDate;
-  //         }
-  //         nextDate = nextDate.plus({ days: 1 });
-  //       }
-  //     };
+      const getNextDeliveryDateFrom = (date: DateTime): DateTime => {
+        let nextDate = date;
+        while (true) {
+          if (nextDate.weekday === 1 || nextDate.weekday === 4) {
+            return nextDate;
+          }
+          nextDate = nextDate.plus({ days: 1 });
+        }
+      };
 
-  //     if (result.deadLineMax > 0) {
-  //       // Добавить deadLineMax часов и найти следующий день доставки
-  //       const tentativeDate = currentTime.plus({ hours: result.deadLineMax });
-  //       deliveryDate = getNextDeliveryDateFrom(tentativeDate);
-  //     } else {
-  //       // Заказ до или после 14:00
-  //       if (currentTime.hour < 14) {
-  //         const tentativeDate = currentTime.plus({ days: 1 });
-  //         deliveryDate = getNextDeliveryDateFrom(tentativeDate);
-  //       } else {
-  //         const tentativeDate = currentTime.plus({ days: 2 });
-  //         deliveryDate = getNextDeliveryDateFrom(tentativeDate);
-  //       }
-  //     }
+      if (result.deadLineMax > 0) {
+        // Добавить deadLineMax часов и найти следующий день доставки
+        const tentativeDate = currentTime.plus({ hours: result.deadLineMax });
+        deliveryDate = getNextDeliveryDateFrom(tentativeDate);
+      } else {
+        // Заказ до или после 14:00
+        if (currentTime.hour < 14) {
+          const tentativeDate = currentTime.plus({ days: 1 });
+          deliveryDate = getNextDeliveryDateFrom(tentativeDate);
+        } else {
+          const tentativeDate = currentTime.plus({ days: 2 });
+          deliveryDate = getNextDeliveryDateFrom(tentativeDate);
+        }
+      }
 
-  //     return deliveryDate;
-  //   },
-  // },
+      return deliveryDate;
+    },
+  },
   {
     supplierName: 'turboCars',
     workingDays: [1, 2, 3, 4, 5, 6], // понедельник - суббота
