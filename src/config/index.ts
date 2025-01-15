daf8b52fe8397975164b6f019f7f6fd4d67bdb4d
dkyzym <kyzym12345@gmail.com>
2025-01-15 12:06:07 +0100

    cors amvera

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
-export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
+export const CLIENT_URL =
+  process.env.CLIENT_URL || 'https://pricer-front.vercel.app';
+// export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
+('https://pricer-front.vercel.app');
 
 export const corsOptions = {
   origin: '*',
