88e4444842458c15bdc01f675b87c066086ad617
dkyzym <kyzym12345@gmail.com>
2025-01-15 14:13:33 +0100

    refactor: ugHeaders

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,10 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
-export const CLIENT_URL =
-  process.env.CLIENT_URL || 'https://pricer-front.vercel.app';
-// export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
-('https://pricer-front.vercel.app');
+export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 
 export const corsOptions = {
   origin: '*',
--- src/constants/headers.ts
+++ src/constants/headers.ts
@@ -1,28 +1,28 @@
-// export const ugHeaders = {
-//   'User-Agent':
-//     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-//   Accept: 'application/json, text/javascript, */*; q=0.01',
-//   'Accept-Encoding': 'gzip, deflate, br, zstd',
-//   'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6,sr;q=0.5',
-//   'X-Requested-With': 'XMLHttpRequest',
-// };
-
 export const ugHeaders = {
-  Host: 'ugautopart.ru',
   'User-Agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
   Accept: 'application/json, text/javascript, */*; q=0.01',
   'Accept-Encoding': 'gzip, deflate, br, zstd',
-  'Accept-Language': 'uk,sq;q=0.9,ru;q=0.8,en-US;q=0.7,en;q=0.6,it;q=0.5',
-  Connection: 'keep-alive',
-  Referer: 'https://ugautopart.ru/',
-  'Sec-Fetch-Dest': 'empty',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Site': 'same-origin',
-  'Sec-CH-UA':
-    '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-  'Sec-CH-UA-Mobile': '?0',
-  'Sec-CH-UA-Platform': '"Windows"',
+  'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6,sr;q=0.5',
   'X-Requested-With': 'XMLHttpRequest',
-  // Cookie будем задавать отдельно через cookieJar (или вручную)
 };
+
+// export const ugHeaders = {
+//   Host: 'ugautopart.ru',
+//   'User-Agent':
+//     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+//   Accept: 'application/json, text/javascript, */*; q=0.01',
+//   'Accept-Encoding': 'gzip, deflate, br, zstd',
+//   'Accept-Language': 'uk,sq;q=0.9,ru;q=0.8,en-US;q=0.7,en;q=0.6,it;q=0.5',
+//   Connection: 'keep-alive',
+//   Referer: 'https://ugautopart.ru/',
+//   'Sec-Fetch-Dest': 'empty',
+//   'Sec-Fetch-Mode': 'cors',
+//   'Sec-Fetch-Site': 'same-origin',
+//   'Sec-CH-UA':
+//     '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+//   'Sec-CH-UA-Mobile': '?0',
+//   'Sec-CH-UA-Platform': '"Windows"',
+//   'X-Requested-With': 'XMLHttpRequest',
+//   // Cookie будем задавать отдельно через cookieJar (или вручную)
+// };
