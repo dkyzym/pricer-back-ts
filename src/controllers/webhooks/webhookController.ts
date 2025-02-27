3e64acb935557eae9363270ac5eda094835aa78c
dkyzym <kyzym12345@gmail.com>
2025-02-27 13:37:59 +0100

    debug: webhook

--- src/controllers/webhooks/webhookController.ts
+++ src/controllers/webhooks/webhookController.ts
@@ -6,18 +6,18 @@ import { verifySignature } from '../../utils/verifySignature.js';
 export const webhookController = (req: Request, res: Response) => {
   // 1) Проверяем подпись
   if (!verifySignature(req)) {
-    logger.warn('Неверная подпись Webhook');
+    console.log('Неверная подпись Webhook');
     return res.status(401).send('Unauthorized');
   }
 
   // 2) Опционально проверим, что это push event
   if (req.headers['x-github-event'] !== 'push') {
-    logger.warn('Not push-событие, игнорируем');
+    console.log('Not push-событие, игнорируем');
     return res.status(200).send('Not a push event');
   }
 
   // 3) Запускаем обновление
-  logger.warn('Получен push Webhook, запускаю обновление...');
+  console.log('Получен push Webhook, запускаю обновление...');
 
   // Команды можно выполнять одной строкой или по отдельности
   // В примере ниже запускаем последовательность команд в child_process
@@ -30,9 +30,9 @@ export const webhookController = (req: Request, res: Response) => {
         logger.error('Ошибка при выполнении команд:', err);
         return res.status(500).send(`Ошибка деплоя ${err} ${stderr}`);
       }
-      logger.info('Вывод команд:', JSON.stringify(stdout));
+      console.log('Вывод команд:', JSON.stringify(stdout));
       console.error('Ошибки команд:', stderr);
-      logger.info('Деплой успешно завершён');
+      console.log('Деплой успешно завершён');
       return res.status(200).send('Updated');
     }
   );
