06509be2f9184ab573c5565c4c37732e4e1db0f5
dkyzym <kyzym12345@gmail.com>
2025-02-27 16:17:44 +0100

    debug: webhook 2

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -32,7 +32,7 @@ export class NotFoundError extends BaseError {
 
 export class RouteNotFoundError extends BaseError {
   constructor() {
-    super('Route not found 1 ');
+    super('Route not found 2 ');
 
     this.code = 404;
   }
