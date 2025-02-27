16f5ec2ff67e962a96a6da25ce0af5f83f43b15d
dkyzym <kyzym12345@gmail.com>
2025-02-27 14:04:24 +0100

    debug: webhook changed log to test

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -32,7 +32,7 @@ export class NotFoundError extends BaseError {
 
 export class RouteNotFoundError extends BaseError {
   constructor() {
-    super('Route not found :( ');
+    super('Route not found :) ');
 
     this.code = 404;
   }
