49e83da40df025e020f0a7025866671f796e3e7e
dkyzym <kyzym12345@gmail.com>
2025-02-27 12:50:11 +0100

    refactor: webhook logs

--- src/controllers/webhooks/webhookController.ts
+++ src/controllers/webhooks/webhookController.ts
@@ -1,22 +1,23 @@
 import { exec } from 'child_process';
 import { Request, Response } from 'express';
 import { verifySignature } from '../../utils/verifySignature.js';
+import { logger } from '../../config/logger/index.js';
 
 export const webhookController = (req: Request, res: Response) => {
   // 1) Проверяем подпись
   if (!verifySignature(req)) {
-    console.log('Неверная подпись Webhook');
+    logger.warn('Неверная подпись Webhook');
     return res.status(401).send('Unauthorized');
   }
 
   // 2) Опционально проверим, что это push event
   if (req.headers['x-github-event'] !== 'push') {
-    console.log('Не push-событие, игнорируем');
+    logger.warn('Not push-событие, игнорируем');
     return res.status(200).send('Not a push event');
   }
 
   // 3) Запускаем обновление
-  console.log('Получен push Webhook, запускаю обновление...');
+  logger.warn('Получен push Webhook, запускаю обновление...');
 
   // Команды можно выполнять одной строкой или по отдельности
   // В примере ниже запускаем последовательность команд в child_process
@@ -25,12 +26,12 @@ export const webhookController = (req: Request, res: Response) => {
     { cwd: 'D:/projects/pricer-back-ts' },
     (err, stdout, stderr) => {
       if (err) {
-        console.error('Ошибка при выполнении команд:', err);
+        logger.error('Ошибка при выполнении команд:', err);
         return res.status(500).send('Ошибка деплоя');
       }
-      console.log('Вывод команд:', stdout);
-      console.error('Ошибки команд:', stderr);
-      console.log('Деплой успешно завершён');
+      logger.info('Вывод команд:', stdout);
+      logger.error('Ошибки команд:', stderr);
+      logger.info('Деплой успешно завершён');
       return res.status(200).send('Updated');
     }
   );
