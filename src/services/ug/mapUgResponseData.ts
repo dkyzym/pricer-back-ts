d86c9b8fb93282a7ee5b269401994ed6e37c02a4
dkyzym <kyzym12345@gmail.com>
2025-03-06 11:33:24 +0100

    fix: ug warehouses

--- src/services/ug/mapUgResponseData.ts
+++ src/services/ug/mapUgResponseData.ts
@@ -10,31 +10,42 @@ export const mapUgResponseData = (
   data: ugArticleSearchResult[],
   brand: string
 ): SearchResultsParsed[] => {
-  const mappedResponseData: SearchResultsParsed[] = data.map((item) => ({
-    id: uuidv4(),
-    article: item.number,
-    brand: item.brand,
-    description: item.description,
-    availability: item.availability, //отрицательные значения -1, -2 и -3 означают "неточное" наличие, которое на сайте выглядит как "+", "++" и "+++" соответственно. Отрицательное значение -10 означает наличие "под заказ".
-    price: item.price,
-    warehouse: item.supplierDescription,
-    imageUrl: '',
-    deadline: item.deliveryPeriod || 1,
-    deadLineMax: item.deliveryPeriodMax || 1,
-    supplier: 'ug',
-    probability: item.deliveryProbability,
-    needToCheckBrand: !isBrandMatch(brand, item.brand),
-    returnable: Number(!item.noReturn),
-    multi: item.packing || 1,
-    allow_return: String(!item.noReturn),
-    warehouse_id: String(item.supplierCode), //supplierCode
-    inner_product_code: item.itemKey, //itemKey
+  const mappedResponseData: SearchResultsParsed[] = data.map((item) => {
+    const ownWarehouses = ['краснодар', 'ростов']; // считаем что это родные склады ЮГ
+    // Приводим описание поставщика к нижнему регистру для корректного поиска
+    const supplierDescriptionLower = item.supplierDescription.toLowerCase();
+    // Если в строке содержится "краснодар" или "ростов", то probability будет 95 процентов
+    const probability = ownWarehouses.some((warehouse) =>
+      supplierDescriptionLower.includes(warehouse)
+    )
+      ? 95
+      : item.deliveryProbability;
 
-    ug: {
-      itemKey: item.itemKey,
-      supplierCode: String(item.supplierCode),
-    },
-  }));
+    return {
+      id: uuidv4(),
+      article: item.number,
+      brand: item.brand,
+      description: item.description,
+      availability: item.availability, // отрицательные значения: -1, -2, -3 означают "неточное" наличие, а -10 — наличие "под заказ".
+      price: item.price,
+      warehouse: item.supplierDescription,
+      imageUrl: '',
+      deadline: item.deliveryPeriod || 1,
+      deadLineMax: item.deliveryPeriodMax || 1,
+      supplier: 'ug',
+      probability,
+      needToCheckBrand: !isBrandMatch(brand, item.brand),
+      returnable: Number(!item.noReturn),
+      multi: item.packing || 1,
+      allow_return: String(!item.noReturn),
+      warehouse_id: String(item.supplierCode),
+      inner_product_code: item.itemKey,
+      ug: {
+        itemKey: item.itemKey,
+        supplierCode: String(item.supplierCode),
+      },
+    };
+  });
 
   const ugResultsWithDeliveryDate = mappedResponseData.map((result) => {
     const deliveryDate = calculateDeliveryDate(result);
