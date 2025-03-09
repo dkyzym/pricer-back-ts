2444d03d566d191ef8fc94eeb2ac72b9c07449f1
dkyzym <kyzym12345@gmail.com>
2025-03-09 09:37:12 +0300

    refactor: cors

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
-export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
+export const CLIENT_URL = [
+  `${process.env.CLIENT_URL}`,
+  `${process.env.HTTP_SITE}`,
+  `${process.env.HTTPS_SITE}`,
+  'http://localhost:3000',
+];
 
 export const corsOptions = {
   origin: '*',
