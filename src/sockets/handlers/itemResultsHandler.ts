a09a15a4f4b7ef2458f04be4bc0f2446a8f0b3da
dkyzym <kyzym12345@gmail.com>
2025-10-24 16:23:27 +0200

    chore: moved readme.md

--- src/sockets/handlers/itemResultsHandler.ts
+++ src/sockets/handlers/itemResultsHandler.ts
@@ -3,21 +3,11 @@ import { Logger } from 'winston';
 import { getItemResultsParams } from '../../types/search.types.js';
 import { universalSupplierHandler } from './universalSupplierHandler.js';
 
-/**
- * Обновленная фабричная функция.
- * Теперь она просто возвращает асинхронную функцию, которая вызывает
- * новый универсальный обработчик. Вся сложная логика инкапсулирована в нем.
- */
 export const createItemResultsHandler = (
   socket: Socket,
   userLogger: Logger
 ) => {
-  /**
-   * Возвращаем функцию, которая полностью соответствует старому контракту,
-   * но внутри себя просто вызывает новый универсальный обработчик.
-   */
   return async (data: getItemResultsParams) => {
-    // Вся логика из десятков строк кода сжимается до одного вызова:
     await universalSupplierHandler(socket, data, userLogger);
   };
 };
